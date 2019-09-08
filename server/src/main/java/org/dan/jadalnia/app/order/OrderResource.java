6db522270831815c3dba0fb15de1b24b75b80f3c
Daniil Iaitskov <dyaitskov@gmail.com>
2019-09-08 12:57:01 -0700

    master clean up

--- server/src/main/java/org/dan/jadalnia/app/order/OrderResource.java
+++ server/src/main/java/org/dan/jadalnia/app/order/OrderResource.java
@@ -14,31 +14,11 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 @Path("/")
 @Produces(APPLICATION_JSON)
 public class OrderResource {
-    public static final String MATCH_RESULT = "/match/result/";
-    public static final String MATCH_LIST_PLAYED_ME = "/match/list/played-by-me/";
-    public static final String MATCH_LIST_JUDGED = "/match/list/judged/";
-    public static final String MY_PENDING_MATCHES = "/match/list/my/pending/";
-    public static final String BID_PENDING_MATCHES = "/match/list/bid/pending/";
-    public static final String OPEN_MATCHES_FOR_JUDGE = "/match/judge/list/open/";
-    public static final String SCORE_SET = "/match/participant/score";
-    public static final String MATCH_WATCH_LIST_OPEN = "/match/watch/list/open/";
-    public static final String MATCH_RESET_SET_SCORE = "/match/reset-set-score";
-    public static final String TID_JP = "{tid}";
-    public static final String BID_JP = "{bid}";
-    public static final String UID = "uid";
-    private static final String MATCH_RULES = "/match/rules/";
-    private static final String MID = "mid";
-    private static final String MID_JP = "{mid}";
-    public static final String MATCH_FOR_JUDGE = "/match/for-judge/";
-    public static final String RESCORE_MATCH = "/match/rescore-match";
-    public static final String MATCH_FIND_BY_PARTICIPANTS = "/match/find-by-participants/";
-
     @Inject
     private AuthService authService;
 
     @Inject
     private OrderService orderService;
-
 
     @Inject
     private Clocker clocker;
