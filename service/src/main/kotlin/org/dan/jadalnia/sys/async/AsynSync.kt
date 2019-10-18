f791daee72c228d6cb675b29b8c095f20d40316c
Daniil Iaitskov <dyaitskov@gmail.com>
2019-10-18 16:37:43 -0700

    class-collision no overlapping classes

--- service/src/main/kotlin/org/dan/jadalnia/sys/async/AsynSync.kt
+++ service/src/main/kotlin/org/dan/jadalnia/sys/async/AsynSync.kt
@@ -4,16 +4,15 @@ import javax.ws.rs.container.AsyncResponse
 import java.util.concurrent.CompletableFuture
 
 class AsynSync {
-    fun <T> sync(
-            resultFuture: CompletableFuture<T>,
-            response: AsyncResponse ) {
-        resultFuture.whenComplete(
-                { result, e ->
-                    if (e == null) {
-                        response.resume(result)
-                    } else {
-                        response.resume(e)
-                    }
-                })
+  fun <T> sync(
+      resultFuture: CompletableFuture<T>,
+      response: AsyncResponse ) {
+    resultFuture.whenComplete { result, e ->
+      if (e == null) {
+        response.resume(result)
+      } else {
+        response.resume(e)
+      }
     }
+  }
 }
