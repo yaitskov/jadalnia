ed23fdcd7c4866e24a1fe8a0b3d0745ef96ab133
Daniil Iaitskov <dyaitskov@gmail.com>
2019-10-23 13:02:35 -0700

    update-menu handle error on listing menu

--- ui/src/app/page/festival/menu/update-fest-menu-item.tsx
+++ ui/src/app/page/festival/menu/update-fest-menu-item.tsx
@@ -2,14 +2,13 @@ import { h } from 'preact';
 import {Container, FwdContainer} from 'injection/inject-1k';
 import { regBundleCtx } from 'injection/bundle';
 import { Instantiable } from 'collection/typed-object';
-import { T } from 'i18n/translate-tag';
 import { TitleStdMainMenu } from 'app/title-std-main-menu';
 import { FestMenuItemForm } from 'app/page/festival/menu/fest-menu-item-form';
 import { TransCom, TransComS } from 'i18n/trans-component';
 import { SecCon } from 'app/component/section-container';
 import { Opt, nic, opt } from 'collection/optional';
-import {FestMenuItemFull, emptyFestMenuItem, MenuItemView} from 'app/service/fest-menu-types';
-
+import {FestMenuItemFull, MenuItemView} from 'app/service/fest-menu-types';
+import { RestErrCo } from "component/err/error";
 import {goBack} from "util/routing";
 import {FestMenuSr} from "app/service/fest-menu-service";
 import {Thenable} from "async/abortable-promise";
@@ -18,6 +17,7 @@ import {Fid} from "app/page/festival/festival-types";
 export interface FestMenuItemS extends TransComS {
   menuItem: Opt<FestMenuItemFull>
   items: Opt<MenuItemView[]>
+  e?: Error
 }
 
 class UpdateFestMenuItem extends TransCom<{fid: Fid, name: string}, FestMenuItemS> {
@@ -38,6 +38,7 @@ class UpdateFestMenuItem extends TransCom<{fid: Fid, name: string}, FestMenuItem
       lst => this.ust(st => ({...st,
           menuItem: opt(lst.find(item => item.name == this.props.name)),
           items: opt(lst)})))
+      .ctch(e => this.ust(st => ({...st, e: e})))
   }
 
   updateMenu(item: FestMenuItemFull): Thenable<void> {
@@ -45,14 +46,15 @@ class UpdateFestMenuItem extends TransCom<{fid: Fid, name: string}, FestMenuItem
       .tn(success => goBack());
   }
 
-  render() {
-    const [TI, TitleStdMainMenuI, FestMenuItemFormI]
-    = this.c3(T, TitleStdMainMenu, FestMenuItemForm);
+  render(p, st) {
+    const [TitleStdMainMenuI, FestMenuItemFormI]
+    = this.c2(TitleStdMainMenu, FestMenuItemForm);
     return <div>
       <TitleStdMainMenuI t$title="Update menu item"/>
       <SecCon>
+        <RestErrCo e={st.e} />
         {
-          this.st.menuItem.ifVE(
+          st.e || this.st.menuItem.ifVE(
             item => <FestMenuItemFormI menuItem={item}
                                        t$submitLabel="Update"
                                        onSubmit={item => this.updateMenu(item)} />,
--- ui/src/app/service/fest-menu-service.tsx
+++ ui/src/app/service/fest-menu-service.tsx
@@ -1,4 +1,3 @@
-import { geT } from 'async/abortable-fetch';
 import { Thenable } from 'async/abortable-promise';
 import { Fid } from 'app/page/festival/festival-types';
 import { MenuItemView, FestMenuItemFull, emptyFestMenuItem } from 'app/service/fest-menu-types';
@@ -9,8 +8,7 @@ export class FestMenuSr {
   private $restSr: RestSr;
 
   list(fid: Fid): Thenable<MenuItemView[]> {
-    return geT(`/api/festival/menu/${fid}`).tn(
-        r => r.json().then(lst => lst));
+    return this.$restSr.geT(`/api/festival/menu/${fid}`);
   }
 
   newFestMenuItem(): FestMenuItemFull {
