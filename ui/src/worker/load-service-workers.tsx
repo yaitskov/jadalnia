c5ed560e76ae681536dca15eef72b28f25feead2
Daniil Iaitskov <dyaitskov@gmail.com>
2019-10-27 15:37:48 -0700

    web-push move push subscription

--- ui/src/worker/load-service-workers.tsx
+++ ui/src/worker/load-service-workers.tsx
@@ -1,10 +1,70 @@
+function sendSubscriptionToServer(subscription) {
+  let key = subscription.getKey ? subscription.getKey('p256dh') : '';
+  let auth = subscription.getKey ? subscription.getKey('auth') : '';
 
+  console.info(`Send key ${key} and auth ${auth} to server`);
+}
+
+function subscribe() {
+  console.log('lets subscribe');
+  navigator.serviceWorker.ready.then(serviceWorkerRegistration => {
+    console.log('sw ready for making subscribe');
+    serviceWorkerRegistration.pushManager
+      .subscribe({userVisibleOnly: true})
+      .then(
+        subscription => {
+          console.log(`have subscription ${subscription}`);
+          return sendSubscriptionToServer(subscription)
+        })
+      .catch(e => {
+        if (Notification.permission == 'denied') {
+          console.warn('Permission for notification was denied');
+        } else {
+          console.log(`Unable to subscribe to push ${e}`);
+        }
+      });
+  }).catch(e => {
+    console.info(`sw is not ready for subscribe due ${e}`);
+  });
+}
 
 export const loadServiceWorkers = () => window.addEventListener('load', () => {
   navigator.serviceWorker.register('/sw.bundle.js')
-    .then(registration => {
-      console.info(`Service worker is registered ${registration}`)
-    }, err => {
-      console.error(`Failed to register service worker ${err}`)
-    });
+    .then(
+      registration => {
+        console.info(`Service worker is registered ${registration}`)
+
+        if (!('showNotification' in ServiceWorkerRegistration.prototype)) {
+          console.warn("notifications are not supported");
+          return;
+        }
+
+        if (Notification.permission  == 'denied') {
+          console.warn("Notifications are disabled");
+          return;
+        }
+
+        if (typeof PushManager == "undefined") {
+          console.warn("Push messaging is not supported");
+          return;
+        }
+
+        navigator.serviceWorker.ready.then(
+          serviceWorkerRegistration => {
+            console.log(`sw ready`);
+            serviceWorkerRegistration.pushManager.getSubscription()
+              .then(subscription => {
+                if (!subscription) {
+                  subscribe();
+                  return;
+                }
+                sendSubscriptionToServer(subscription);
+              }).catch(e => console.info(`Error during subscription ${e}`))
+          }).catch(e => {
+          console.info(`sw not ready ${e}`);
+        });
+
+      }, err => {
+        console.info(`Failed to register service worker ${err}`)
+      });
 });
--- ui/src/worker/worker-101.tsx
+++ ui/src/worker/worker-101.tsx
@@ -1,6 +1,54 @@
 import {localDateYmd, roundToMinutes} from "util/my-time";
 
+self.addEventListener('message', event => {
+  console.log(`sw message event ${event}`);
+});
+
+self.addEventListener('push', event => {
+  console.log(`sw push event ${event}`);
+
+  // show notification
+});
+
+self.addEventListener('pushsubscriptionchange', event => {
+  console.log(`sw push subscription change event ${event}`);
+});
+
+self.addEventListener('sync', event => {
+  console.log(`sw sync ${event}`);
+});
+
+self.addEventListener('online', event => {
+  console.log(`sw online ${event}`);
+});
+
+self.addEventListener('offline', event => {
+  console.log(`sw offline ${event}`);
+});
+
+self.addEventListener('pagehide', event => {
+  console.log(`sw pagehide ${event}`);
+});
+
+self.addEventListener('pageshow', event => {
+  console.log(`sw pageshow ${event}`);
+});
+
+self.addEventListener('unload', event => {
+  console.log(`sw unload ${event}`);
+});
+
+self.addEventListener('activate', event => {
+  console.log(`sw activate event ${event}`);
+});
+
+self.addEventListener('fetch', event => {
+  console.log(`sw fetch event ${event}`);
+});
+
 self.addEventListener('install', (event) => {
   let ts = new Date();
   console.log(`Start install worker 101. Version 2. ${roundToMinutes(localDateYmd(ts))}`);
+
+  // init cache
 });
