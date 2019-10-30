1fb14a6d498a4f2be1de61df7b19858e0c096f01
Daniil Iaitskov <dyaitskov@gmail.com>
2019-10-30 16:30:36 -0700

    list-volunteers copy link

--- ui/src/component/copy-click-link.tsx
+++ ui/src/component/copy-click-link.tsx
@@ -1,27 +1,36 @@
 import { h } from 'preact';
 import {Link} from "preact-router";
 import {MyCo} from "component/my-component";
+import copy from 'clipboard-copy';
+import {siteUrl, isAbsUrl} from "util/routing";
 
 export interface CpClickLnkP {
   url: string;
   t$lbl: string;
   classes?: string;
+  onCopied?: () => void;
+  onFailed?: (e: Error) => void;
 }
 
-export class CpClickLnk extends MyCo<CpClickLnkP, {}> {
+export class CpClickLnk extends MyCo<CpClickLnkP, {absUrl: string}> {
   constructor(props) {
     super(props);
+    let url = props.url;
+    this.st = {absUrl: isAbsUrl(url) ? url : siteUrl() + url };
     this.onClick = this.onClick.bind(this);
   }
 
   onClick(e) {
     e.preventDefault();
     e.stopImmediatePropagation();
+    copy(this.st.absUrl)
+      .then(this.props.onCopied || (() => {}))
+      .catch(this.props.onFailed || ((_: Error) => {}));
     return false;
   }
 
-  render(p) {
-    return <Link href={p.url}
+  render(p, st) {
+    return <Link href={st.absUrl}
                  title="click to copy"
                  class={p.classes || ''}
                  onClick={this.onClick}>
--- ui/src/util/routing.tsx
+++ ui/src/util/routing.tsx
@@ -1,1 +1,3 @@
 export const goBack = () => window.history.go(-1);
+export const siteUrl = () => location.href.substring(0, location.href.indexOf('/', 10));
+export const isAbsUrl = (url: string) => url.match(/^http/);
