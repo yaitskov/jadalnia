92749457b4a8222ed3a4176d5c98dee27ba5703a
Daniil Iaitskov <dyaitskov@gmail.com>
2019-11-01 17:11:35 -0700

    volunteer-reg inject web push properties

--- run-classes.sh
+++ run-classes.sh
@@ -2,4 +2,6 @@
 
 export MAVEN_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=${PORT:-5001},server=y,suspend=n"
 
-mvn exec:java -pl :server -Djadalina.http.port=${HTTP_PORT:-8280} -Dexec.mainClass=org.dan.jadalnia.StartJadalniaKt
+mvn exec:java -pl :server -Djadalina.http.port=${HTTP_PORT:-8280} \
+    -Dweb.push.key.private=$WEB_PUSH_KEY_PRIVATE \
+    -Dexec.mainClass=org.dan.jadalnia.StartJadalniaKt
--- run-jar.sh
+++ run-jar.sh
@@ -4,4 +4,5 @@ export JVM_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_s
 
 java -Djadalina.http.port=${HTTP_PORT:-8280} \
      $JVM_OPTS \
+     -Dweb.push.key.private=$WEB_PUSH_KEY_PRIVATE \
      -jar $(dirname $0)/server/target/server-1.0.0-SNAPSHOT.jar \
