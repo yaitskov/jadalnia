212bf748fc5ef1e3dfb1a20a743cd1a0ac53267b
Daniil Iaitskov <rtfm.rtfm.rtfm@gmail.com>
2020-01-15 10:41:10 -0800

    master remove dead code

--- server/src/main/java/org/dan/jadalnia/util/FuncUtils.java
+++
@@ -1,8 +0,0 @@
-package org.dan.jadalnia.util;
-
-import java.util.function.BiFunction;
-
-public class FuncUtils {
-    public static final BiFunction<Integer, Integer, Integer> SUM_INT
-            = (a, b) -> a + b;
-}
--- server/src/main/java/org/dan/jadalnia/util/TriFunc.java
+++
@@ -1,5 +0,0 @@
-package org.dan.jadalnia.util;
-
-public interface TriFunc<A, B, C, R> {
-    R apply(A a, B b, C c);
-}
